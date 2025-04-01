73c9eb1d830539348c886a826c6babc8b1d194e6
Binod Budhathoki <binodbudhathoki@Binods-MacBook-Air.local>
2025-04-01 22:05:24 +0900

    added hono

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+import author from "./author";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.route("/author", author);
+
+export const GET = handle(app);
+export const POST = handle(app);
